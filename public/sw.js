7c6e64433a8f7331608e8a7373edd5324d6d386c
RyanFin <ryanfinlayson125@hotmail.com>
2019-03-06 18:14:12 +0000

    Add Cache Storage mechanism and static assets to sw.js (Service Worker)
    • Need to make cache storage work offline

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,17 @@
-self.addEventListener('install', event =>{
-	console.log('install')
+const staticAssets = [
+	'./',
+	'./stylesheets/style.css'
+
+];
+
+
+
+self.addEventListener('install', async event =>{
+	// console.log('install');
+	const cache = await caches.open('aict-static');
+	cache.addAll(staticAssets);
 });
 
 self.addEventListener('fetch', event =>{
-	console.log('fetch')
+	console.log('fetch');
 });
