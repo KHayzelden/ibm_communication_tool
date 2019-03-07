661908065174d15f8a9996c636afe21bd802804a
ednut15 <babatundeadeola@gmail.com>
2019-03-07 15:01:41 +0000

    added appid

--- app.js
+++ app.js
@@ -8,6 +8,10 @@
 // for more info, see: http://expressjs.com
 var exports = module.exports = {};
 var express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const WebAppStrategy = require("ibmcloud-appid").WebAppStrategy;
+const CALLBACK_URL = "/ibm/cloud/appid/callback";
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -24,7 +28,6 @@ var cloudant, useDb;
 var vcapLocal;
 try {
     vcapLocal = require('./vcap-local.json');
-    console.log("Loaded local VCAP", JSON.stringify(vcapLocal));
 } catch (e) { }
 
 const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {};
@@ -53,17 +56,19 @@ if(cloudant != null) {
     //database name
     var dbName = 'watsoncommdb';
 
-    // cloudant.db.list(function(err, allDbs) {
-    //     console.log('All my databases: %s', allDbs.join(', '))
-    // });
-    // // Create a new "mydb" database.
-    // cloudant.db.create(dbName, function(err, data) {
-    //     if(!err) //err if database doesn't already exists
-    //         console.log("Created database: " + dbName);
-    //     else
-    //         console.log(err);
-    //
-    // });
+    cloudant.db.list(function(err, allDbs) {
+        console.log('All my databases: %s', allDbs.join(', '));
+        if(! (allDbs.indexOf(dbName) > -1)) {
+            // Create a new "mydb" database.
+            cloudant.db.create(dbName, function(err, data) {
+                if(!err) //err if database doesn't already exists
+                    console.log("Created database: " + dbName);
+                else
+                    console.log(err);
+
+            });
+        }
+    });
     // Specify the database we are going to use (mydb)...
     exports.useDb = cloudant.db.use(dbName);
     // console.log(useDb);
@@ -83,12 +88,59 @@ app.use (function (req, res, next) {
 // requiring routes
 var index = require('./routes/index');
 
+app.use(session({
+    secret: "123456",
+    resave: true,
+    saveUninitialized: true
+}));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Configure express application to use passportjs
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Configure passportjs with user serialization/deserialization. This is required
+// for authenticated session persistence accross HTTP requests.
+passport.serializeUser(function(user, cb) {
+    cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+});
+
+// load localdev configuration and service credentials
+var localdev;
+try {
+    localdev = require('./localdev-config.json');
+} catch (e) { }
+
+passport.use(new WebAppStrategy({
+    tenantId: localdev["tenantId"],
+    clientId: localdev["clientId"],
+    secret: localdev["secret"],
+    oauthServerUrl: localdev["oauthServerUrl"],
+    redirectUri: appEnv.url + CALLBACK_URL
+}));
+
+// Callback to finish the authorization process. Will retrieve access and identity tokens/
+// from AppID service and redirect to either (in below order)
+// 1. the original URL of the request that triggered authentication, as persisted in HTTP session under WebAppStrategy.ORIGINAL_URL key.
+// 2. successRedirect as specified in passport.authenticate(name, {successRedirect: "...."}) invocation
+// 3. application root ("/")
+app.get(CALLBACK_URL, passport.authenticate(WebAppStrategy.STRATEGY_NAME));
+
+
+app.get('/protected', passport.authenticate(WebAppStrategy.STRATEGY_NAME), function(req, res) {
+    res.json(req.user);
+});
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
+
 // parse application/json
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
