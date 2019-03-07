c97c51b1f7bbda236f270e7796e0bca49e9f9227
RyanFin <ryanfinlayson125@hotmail.com>
2019-03-07 20:09:54 +0000

    Add New Pages to Cache

---
+++ public/assets/js/bookmark.js
@@ -0,0 +1,172 @@
+$("#editbutton").click(function(){
+ 
+    console.log($(this).html());
+    if($(this).html() == "Edit")
+    {
+        $("#editbutton").text("Cancel");
+        $("#saveButton").attr("style","display:inline-block");
+        $("#editCaption").attr("style","display:block");
+        $("#deleteButton").attr("style","display:inline-block");
+        $(".input").attr("style","display:inline-block");
+    }
+    else if($(this).html() == "Cancel")
+    {
+        $("#editbutton").text("Edit");
+        $("#saveButton").attr("style","display:none");
+        $("#editCaption").attr("style","display:none");
+        $("#deleteButton").attr("style","display:none");
+        $(".input").attr("style","display:none");
+    }
+    
+    });
+
+    $("#editbutton1").click(function(){
+ 
+        console.log($(this).html());
+        if($(this).html() == "Edit")
+        {
+            $("#editbutton1").text("Cancel")       
+            $("#saveButton1").attr("style","display:inline-block");
+            $("#editCaption1").attr("style","display:block");
+            $("#deleteButton1").attr("style","display:inline-block");
+            $(".input1").attr("style","display:inline-block");
+        }
+        else if($(this).html() == "Cancel")
+        {
+            $("#editbutton1").text("Edit");
+            $("#saveButton1").attr("style","display:none");
+            $("#editCaption1").attr("style","display:none");
+            $("#deleteButton1").attr("style","display:none");
+            $(".input1").attr("style","display:none");
+        }
+        
+        });
+
+$('#deleteButton').click(function(){
+        copyText($(this));
+})
+        
+$('#deleteButton1').click(function(){
+        copyText($(this));
+})
+  
+$('#clearButton').click(function(){
+
+    if($(this).html() == "Clear History")
+    {
+        swal({
+            title: "Are you sure?",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        })
+    .then((willDelete) => {
+            if (willDelete) 
+            {
+                swal("Successfully Deleted!", {
+                icon: "success",
+                });
+            } else 
+            {
+                swal("Your History is safe!");
+            }
+        });
+    }
+    else if($(this).html() == "Back")
+    {
+        $("#showdate").text("History");
+        $("#clearButton").text("Clear History");
+    }
+ 
+})
+
+clearButton
+function copyText(butt){
+    swal({
+            title: "Are you sure?",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        })
+    .then((willDelete) => {
+            if (willDelete) 
+            {
+                butt.parent().next().children().each(function(){
+                    if($(this).children(":first-child").children(":checked").length) {
+                        $(this).remove();
+                        $("#editbutton").text("Edit");
+                        $("#saveButton").attr("style","visibility:hidden");
+                        $("#deleteButton").attr("style","visibility:hidden");
+                        $(".input-group-addon").attr("style","visibility:hidden");
+                    }
+                });
+                swal("Successfully Deleted!", {
+                icon: "success",
+                });
+            } else 
+            {
+                swal("Your Bookmark is safe!");
+            }
+        });
+}
+
+$('#list-example1').click(function(){
+    var html = '<span class="input-group-addon" style = "visibility:hidden"><input type="checkbox"></span></a>';
+    inText($(this), html);
+})
+
+$('#list-example2').click(function(){
+    var html = '<span class="input1 input-group-addon" style = "visibility:hidden"><input type="checkbox"></span></a>';
+    inText($(this), html);
+})
+
+function inText(butt, html){
+    swal("Enter new sentences:", {
+        content: "input",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((value) => {
+        if(value)
+        {
+            console.log(value);
+            html = '<a class="list-group-item  d-flex justify-content-between align-items-center draggable="true"" href="#list-item-1">' + value + html;
+            var source = $( html );
+            bindDrag(source);
+            source.appendTo( butt.parent().prev() ); 
+            swal("Successfully Added");
+        }
+       
+      });
+}
+
+var iosDragDropShim = { enableEnterLeave: true } 
+var dragElement = null;
+function bindDrag(source) {
+    source.on('dragstart', function (event) {
+        console.log(1);
+        dragElement = $(this);
+        $(this).css("background-color","#f8f8f8");
+    });
+
+    source.on('dragend', function (event) {
+        console.log(2);
+        $(event.target).css("background-color","#fff");
+        event.preventDefault();
+    });
+
+    source.on('dragenter', function (event) {
+        if($(this).prev().is(dragElement)){
+            dragElement.insertAfter($(this)); 
+        } else if($(this).next().is(dragElement)){
+            dragElement.insertBefore($(this)); 
+        }
+    });
+}
+document.ondragover = function(e){e.preventDefault();}          // 必须设置dragover阻止默认事件
+document.ondrop = function(e){e.preventDefault();}
+
+var source = $('.list-group-item');
+source.each(function(){
+    bindDrag($(this));
+});
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,82 @@
 var express = require('express');
 var router = express.Router();
-var app = require('../app');
+var app = require('../app.js');
 var bodyParser= require("body-parser");
 
 router.get('/', function(req, res, next) {
     var names = [];
-    if(!app.useDb) {
-        res.render('index', {title: 'Watson Titter Communication',users: names});
+    if(!app.db) {
+        console.log(names);
+        res.render('index', {title: 'Watson Titter Communication',user: names});
         return;
     }
-    app.useDb.list({ include_docs: true }, function(err, body) {
+    app.db.list({ include_docs: true }, function(err, body) {
         if (!err) {
-            console.log(body);
             body.rows.forEach(function(row) {
-                console.log(row.doc);
                 if(row.doc.name)
                     names.push(row.doc.name);
             });
-            res.render('index', {title: 'Watson Titter Communication', users: names});
+            console.log(names);
+            res.render('index', {title: 'Watson Titter Communication', user: names});
         }
     });
+
+});
+
+router.get('/search', function(req, res, next) {
+    var searchResults = [];
+    var bookmarks1a = [];
+    var bookmarks2a = [];
+
+    
+
+    // var data = '{"searchRTest": "Laborum in proident eu ad laboris incididunt Lorem sit amet ipsum"}';
+
+    // var json = JSON.parse(data);
+
+    // searchResults.push(json.searchRTest);
+
+    bookmarks1a.push('Bookmark 1');
+    bookmarks1a.push('Bookmark 2');
+    bookmarks1a.push('Bookmark 3');
+    bookmarks1a.push('Bookmark 4');
+    bookmarks1a.push('Bookmark 5');
+
+    bookmarks2a.push('Bookmark 1');
+    bookmarks2a.push('Bookmark 2');
+    bookmarks2a.push('Bookmark 3');
+    bookmarks2a.push('Bookmark 4');
+    bookmarks2a.push('Bookmark 5');
+    bookmarks2a.push('Bookmark 6');
+    bookmarks2a.push('Bookmark 7');
+    bookmarks2a.push('Bookmark 8');
+    bookmarks2a.push('Bookmark 9');
+    bookmarks2a.push('Bookmark 10');
+
+    if(!app.db) {
+        res.render('search', {title: 'Watson Titter Communication', name: 'Test', results: searchResults, bookmarks1: bookmarks1a, bookmarks2: 
+        bookmarks2a});
+        return;
+    }
+});
+
+router.get('/search_history', function(req, res, next) {
+    res.render('search_history', {title: 'Watson Titter Communication', name: 'Test'});
+
+});
+
+router.get('/account', function(req, res, next) {
+    res.render('account', {title: 'Watson Titter Communication', name: 'Test'});
+
+});
+
+router.get('/bookmarks', function(req, res, next) {
+    res.render('bookmarks', {title: 'Watson Titter Communication', name: 'Test'});
+
+});
+
+router.get('/settings', function(req, res, next) {
+    res.render('settings', {title: 'Watson Titter Communication', name: 'Test'});
 
 });
 
@@ -30,18 +87,19 @@ router.post('/add_name', function (req, res, next)  {
         return;
     }
     var userName = req.body.name;
-    var users = { "name" : userName };
-    if(!app.useDb) {
+    var doc = { "name" : userName };
+    if(!app.db) {
+        console.log("No database.");
         res.render('index', {title: 'Watson Titter Communication'});
         return;
     }
 
-    app.useDb.insert(users, function(err, body, header) {
+    app.db.insert(doc, function(err, body, header) {
         if (err) {
-            console.log('[watsoncommdb.insert] ', err.message);
+            console.log('[mydb.insert] ', err.message);
             res.send("Error");
         }
-        users._id = body.id;
+        doc._id = body.id;
         res.redirect("/");
     });
 });
