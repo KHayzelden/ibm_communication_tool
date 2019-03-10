57f461fee7d23a3b4d9b3e9e3f1fa942317c69f2
fuxiaofeng <jayfely@qq.com>
2019-03-10 15:37:35 +0000

    search history

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -4,11 +4,17 @@ socket.on('show results', (data) =>{
 	var searchJson = data.results;
 	var ul = document.getElementById("search_result_li");
 
+	while(document.getElementById("result_list") != null){
+	    var item = document.getElementById("result_list");
+	    ul.removeChild(item);
+	}
+
 	for(var i = 0; i < searchJson.length; i++){
 		var searchArray = searchJson[i].searchRTest;
 
 		for(var j = 0; j < searchArray.length; j++){
 			var li = document.createElement('a');
+			li.setAttribute("id", "result_list");
 			li.appendChild(document.createTextNode(searchArray[j]));
 			li.className = "list-group-item  d-flex justify-content-between align-items-center";
 			ul.appendChild(li);
---
+++ public/assets/js/search_history.js
@@ -0,0 +1,65 @@
+ var socket = io();
+socket.on('show history', (data) =>{
+	var searchJson = data.results;
+	refresh_history_list(searchJson);
+}); 
+
+// $( window ).load(function() {
+// 	// new the history list
+// });
+
+
+function refresh_history_list(data){
+	var container = document.getElementById("history_list");
+	
+	while(document.getElementById("result_list") != null){
+	    var item = document.getElementById("result_list");
+	    container.removeChild(item);
+	}
+
+	for(var i = 0; i < data.length; i++){
+		// time, keyword, result
+		var time = data[i].time;
+		var keyword = data[i].text;
+		var result = data[i].results;
+
+		var li = document.createElement('li');
+		li.className = "mdui-subheader"
+		li.setAttribute("id", "result_list");
+		li.appendChild(document.createTextNode(time));
+
+		var div = document.createElement('div');
+		div.setAttribute("id", "result_list");
+		div.className = "mdui-panel-item";
+
+		var div2 = document.createElement('div');
+		div2.className = "mdui-panel-item-header";
+		div2.appendChild(document.createTextNode(keyword));
+		div.appendChild(div2);
+
+		var div3 = document.createElement('div');
+		div3.className = "mdui-panel-item-body"
+		var ul = document.createElement('ul');
+		ul.className = "mdui-list-dense";
+		div3.appendChild(ul);
+
+		for(var j = 0; j < result.length; j++){
+			var l = document.createElement('li');
+			l.className = "mdui-list-item mdui-ripple";
+			l.appendChild(document.createTextNode(result[j].text));
+			ul.appendChild(l);
+		}
+		div.appendChild(div3);
+		container.appendChild(li);
+		container.appendChild(div);
+	}
+
+};
+
+socket.on('message', function(data){
+    console.log('Connection successful!');
+});
+
+function show_history(keywords) {
+   socket.emit('search history', keywords);
+};
