1dbbea1d280892931b958d9d3aec5e59aaf7b79b
RyanFin <ryanfinlayson125@hotmail.com>
2019-03-17 16:09:04 +0000

    Only cache project files

--- public/sw.js
+++ public/sw.js
@@ -1,29 +1,97 @@
-const staticAssets = [
-	'./',
-	'./stylesheets/style.css',
-	'./search',
-	'./account',
-	'./bookmarks',
-	'./search_history',
-	'./settings'
-];
+// const staticAssets = [
+// 	'./',
+// 	'./stylesheets/style.css',
+// 	'./search',
+// 	'./account',
+// 	'./bookmarks',
+// 	'./search_history',
+// 	'./settings'
+// ];
 
+
+// Fill here with your cache name-version.
+const CACHE_NAME = 'my-cache-v1'
+// This is the list of URLs to be cached by your Progressive Web App URLs.
+const CACHED_URLS = [
+  // './',
+  './stylesheets/style.css',
+  './search',
+  './account',
+  './bookmarks',
+  './search_history',
+  './settings'
+]
+
+let cache = null
+
+// Open cache on install.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(currentCache => {
+        // Store a reference to current cache, to be used on fetch event handler.
+        cache = currentCache
+
+        cache.addAll(CACHED_URLS)
+      })
+      .then(self.skipWaiting())
+  )
+})
+
+// Handle fetch event with snippet stolen from here: https://www.youtube.com/watch?v=TtXvE814SQA
+self.addEventListener('fetch', event => {
+  const request = event.request
+
+  const networkResponsePromise = fetch(request).catch(ignore => {})
+  const cachedResponsePromise = caches.match(request)
+
+  event.respondWith(async function () {
+    const cacheResponse = await cachedResponsePromise
+    if (cacheResponse) return cacheResponse
+
+    const networkResponse = await networkResponsePromise
+    if (networkResponse) return networkResponse.clone()
+
+    throw new Error(`Neither network nor cache had a response for ${request.url}`)
+  }())
+
+  event.waitUntil(async function () {
+    const networkResponse = await networkResponsePromise
+
+    if (networkResponse) cache.put(request, networkResponse.clone())
+  }())
+})
+
+// Clean up caches other than current.
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          const deleteThisCache = cacheName !== CACHE_NAME
+
+          return deleteThisCache
+        }).map(cacheName => caches.delete(cacheName))
+      )
+    })
+  )
+})
 /*
 ------ STALE-WHILE-REVALIDATE --------
 */
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.open('watsoncomm-dynamic').then(function(cache) {
-      return cache.match(event.request).then(function(response) {
-        var fetchPromise = fetch(event.request).then(function(networkResponse) {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
-        })
-        return response || fetchPromise;
-      })
-    })
-  );
-});
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.open('watsoncomm-dynamic').then(function(cache) {
+//       return cache.match(event.request).then(function(response) {
+//         var fetchPromise = fetch(event.request).then(function(networkResponse) {
+//           cache.put(event.request, networkResponse.clone());
+//           return networkResponse;
+//         })
+//         return response || fetchPromise;
+//       })
+//     })
+//   );
+// });
 
 
 // self.addEventListener('install', async event =>{
