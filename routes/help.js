adb9fb01bd9a18d2f40eb2d9cc0a2aa382f70a81
ucabouj <ucabouj@ucl.ac.uk>
2019-03-19 17:31:36 +0000

    removed testing scripts

--- routes/help.js
+++ routes/help.js
@@ -1,6 +1,12 @@
-module.exports = function(router, app){
-	router.get('/help', function(req, res, next) {
-	    res.render('help', {title: 'Watson Twitter Communication', page_name: 'help', name: 'HSmith', loggedIn: true});
+const passport = require('passport');
+const appID = require("ibmcloud-appid");
 
+const WebAppStrategy = appID.WebAppStrategy;
+const userProfileManager = appID.UserProfileManager;
+const UnauthorizedException = appID.UnauthorizedException;
+
+module.exports = function(router, app){
+	router.get('/help', passport.authenticate(WebAppStrategy.STRATEGY_NAME), function(req, res, next) {
+	    res.render('help', {title: 'Watson Twitter Communication', page_name: 'help', name: 'HSmith', fname: 'Harry', sname: 'Smith', email: 'hsmith@gmail.com', loggedIn: true});
 	});
-}
+};
